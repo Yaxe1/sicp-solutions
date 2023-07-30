8df34836dc403fda970299e6dee186b673204bbf
Yaxel <0x96dece1b5d156@protonmail.com>
2023-07-30 00:41:56 -0700

    Remove old scheme files

--- 1/4.scm
+++
@@ -1,3 +0,0 @@
-; The body of this procedure is a combination whose operator is itself an if-
-; statement. If b > 0, the operator will be "+". Otherwise, it will be "-", in 
-; this way ensuring that a is summed with the absolute value of b.
--- 1/6.scm
+++
@@ -1,4 +0,0 @@
-; This new-if procedure is not a special form but rather a procedure call, and 
-; therefore all of the arguments will be evaluated first before they are passed 
-; to `cond`. This means that the predicate is ignored and the new `sqrt-iter` 
-; procedure will call itself recursively forever.
